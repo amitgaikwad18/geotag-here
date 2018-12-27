5b8d09b24b112e51ffd4bd5ab63b74b2581b4633
Amit Gaikwad <amitgaikwad85@gmail.com>
2018-12-28 02:39:48 +0530

    added Leaflet map

--- src/app/geo-tag/geo-tag.page.ts
+++ src/app/geo-tag/geo-tag.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-declare const H;
+import * as L from 'leaflet';
+
+// declare const H: any;
 
 @Component({
   selector: 'app-geotag',
@@ -10,23 +12,47 @@ declare const H;
 
 export class GeoTagPage implements OnInit {
 
-  herePlatform;
+  platform: any;
 
-  constructor() {
-    this.herePlatform =   new H.service.Platform({
-      'app_id': 'nSmddOydoGMXfBKZyKua',
-      'app_code': 'dwMDkaLW9RWFwkZ5DfR9iA'
-    });
-  }
+  lat: any;
+  lng: any;
+
+  map: any;
+
+  constructor() {}
+
   ngOnInit() {
 
-    const defaultLayers = this.herePlatform.createDefaultLayers();
-    const map = new H.Map(
-      document.getElementById('mapContainer'),
-      defaultLayers.normal.map,
-      {
-        zoom: 10,
-        // center: { lat: 52.5, lng: 13.4 }
-      });
+    // this.geolocation.getCurrentPosition().then((resp) => {
+    //   this.lat = resp.coords.latitude;
+    //   this.lng = resp.coords.longitude;
+    //  }).catch((error) => {
+    //    console.log('Error getting location', error);
+    //  });
+
+    //  let watch = this.geolocation.watchPosition();
+    //  watch.subscribe((data) => {
+    //   // data can be a set of coordinates, or an error (if an error occurred).
+    //   // data.coords.latitude
+    //   // data.coords.longitude
+    //  });
+
+    // let Latlng = L.latLng(19.07, 72.87);
+
+    this.map = L.map('mapid').setView([19.07, 72.87], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    // L.marker(Latlng).addTo(this.map);
+  }
+
+  addMarker() {
+
+    let Latlng = L.latLng(19.07, 72.87);
+
+    L.marker(Latlng).addTo(this.map);
+
   }
 }
