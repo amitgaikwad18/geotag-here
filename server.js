7cee538815ed371d204b7248b91fbd5bced5cbee
Amit GAIKWAD <Amit.GAIKWAD@firmenich.com>
2019-01-03 12:57:06 +0530

    fixed err - PORT is not defined

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use(express.static(path.resolve(__dirname, "www")));
 // app.set('port', process.env.PORT || 5000);
 const port = process.env.PORT || 5000;
 app.listen(port ,function () {
-  console.log('Express server listening on port ' + PORT);
+  console.log('Express server listening on port ' + port);
 });
