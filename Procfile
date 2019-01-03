3fc92567da6b5571fa88e554149bccdf24227f5d
Amit GAIKWAD <Amit.GAIKWAD@firmenich.com>
2019-01-03 12:35:43 +0530

    commit for heroku app run

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run ionic-build && npm start
+web: npm run build && npm start
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app      = express();
 var morgan = require('morgan');            
 var bodyParser = require('body-parser');    
 var cors = require('cors');
+var path = require('path');
  
 app.use(morgan('dev'));                                        
 app.use(bodyParser.urlencoded({'extended':'true'}));            
@@ -16,9 +17,9 @@ app.use(function(req, res, next) {
   next();
 });
  
-app.use(express.static('www'));
+app.use(express.static(path.resolve(__dirname, "www")));
 // app.set('port', process.env.PORT || 5000);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', function () {
+app.listen(PORT, function () {
   console.log('Express server listening on port ' + PORT);
 });
